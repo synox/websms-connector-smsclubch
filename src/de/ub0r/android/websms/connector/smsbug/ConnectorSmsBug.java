be0b1b7f039b2155e3a7d12c645a9022728a720f
Synox <sydub@dubgo.com>
2012-09-02 21:06:47 +0200

    first working version

--- src/de/ub0r/android/websms/connector/smsbug/ConnectorSmsBug.java
+++ src/de/ub0r/android/websms/connector/smsbug/ConnectorSmsBug.java
@@ -1,18 +1,18 @@
 /*
- * Copyright (C) 2010 Felix Bechstein
- * 
- * This file is part of WebSMS.
- * 
+ * Copyright (C) 2012
+ *
+ * This file is part of sms-club.ch Connector for WebSMS by Felix Bechstein.
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; If not, see <http://www.gnu.org/licenses/>.
  */
@@ -20,9 +20,13 @@ package de.ub0r.android.websms.connector.smsbug;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.content.Context;
 import android.content.Intent;
@@ -31,26 +35,28 @@ import android.preference.PreferenceManager;
 import de.ub0r.android.websms.connector.common.Connector;
 import de.ub0r.android.websms.connector.common.ConnectorCommand;
 import de.ub0r.android.websms.connector.common.ConnectorSpec;
+import de.ub0r.android.websms.connector.common.ConnectorSpec.SubConnectorSpec;
 import de.ub0r.android.websms.connector.common.Log;
 import de.ub0r.android.websms.connector.common.Utils;
+import de.ub0r.android.websms.connector.common.Utils.HttpOptions;
 import de.ub0r.android.websms.connector.common.WebSMSException;
-import de.ub0r.android.websms.connector.common.ConnectorSpec.SubConnectorSpec;
 
 /**
  * AsyncTask to manage IO to smsbug.com API.
  * 
- * @author flx
  */
 public class ConnectorSmsBug extends Connector {
+	// see api doc:
+	// http://blog.sms-club.ch/wp-content/uploads/2009/06/sms-club_api_interface.pdf
+
+	private static final Pattern PATTERN_ERR = Pattern.compile("<error id=\"([^\"]+)\"/>");
+	private static final Pattern PATTERN_CREDIT = Pattern.compile("credit=\"([0-9]+)\"");
+
 	/** Tag for output. */
-	private static final String TAG = "smsbug";
+	private static final String TAG = "sms-club.ch";
 
 	/** SmsBug Gateway URL. */
-	private static final String URL_SEND = // .
-	"https://www.smsbug.com/api/webservice.asmx/multiplesms";
-	/** SmsBug Gateway URL. */
-	private static final String URL_BALANCE = // .
-	"https://www.smsbug.com/api/webservice.asmx/checkCredits";
+	private static final String URL_SEND = "https://www.sms-club.ch/api/send";
 
 	/**
 	 * {@inheritDoc}
@@ -60,11 +66,12 @@ public class ConnectorSmsBug extends Connector {
 		final String name = context.getString(R.string.connector_smsbug_name);
 		ConnectorSpec c = new ConnectorSpec(name);
 		c.setAuthor(// .
-				context.getString(R.string.connector_smsbug_author));
+		context.getString(R.string.connector_smsbug_author));
 		c.setBalance(null);
-		c.setCapabilities(ConnectorSpec.CAPABILITIES_UPDATE
-				| ConnectorSpec.CAPABILITIES_SEND
-				| ConnectorSpec.CAPABILITIES_PREFS);
+
+		// balance update without sending is currently not supported by the api,
+		// so it also not supported by the implementation.
+		c.setCapabilities(ConnectorSpec.CAPABILITIES_SEND | ConnectorSpec.CAPABILITIES_PREFS);
 		c.addSubConnector(TAG, name, SubConnectorSpec.FEATURE_MULTIRECIPIENTS);
 		return c;
 	}
@@ -73,10 +80,8 @@ public class ConnectorSmsBug extends Connector {
 	 * {@inheritDoc}
 	 */
 	@Override
-	public final ConnectorSpec updateSpec(final Context context,
-			final ConnectorSpec connectorSpec) {
-		final SharedPreferences p = PreferenceManager
-				.getDefaultSharedPreferences(context);
+	public final ConnectorSpec updateSpec(final Context context, final ConnectorSpec connectorSpec) {
+		final SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(context);
 		if (p.getBoolean(Preferences.PREFS_ENABLED, false)) {
 			if (p.getString(Preferences.PREFS_PASSWORD, "").length() > 0) {
 				connectorSpec.setReady();
@@ -90,7 +95,7 @@ public class ConnectorSmsBug extends Connector {
 	}
 
 	/**
-	 * Check return code from smsbug.com.
+	 * Check return code from sms-club.ch.
 	 * 
 	 * @param context
 	 *            {@link Context}
@@ -104,12 +109,56 @@ public class ConnectorSmsBug extends Connector {
 			return true;
 		} else if (ret < 300) {
 			throw new WebSMSException(context, R.string.error_input);
+		} else if (ret == 401) {
+			throw new WebSMSException(context, R.string.error_pw);
 		} else {
-			if (ret == 401) {
-				throw new WebSMSException(context, R.string.error_pw);
-			}
 			throw new WebSMSException(context, R.string.error_server, // .
 					" " + ret);
+		}
+	}
+
+	private static void checkForErrors(final Context context, final String htmlText) {
+		if (htmlText.contains("<error")) {
+			Log.d(TAG, "response contains <error>");
+
+			// there was an error
+			Matcher m = PATTERN_ERR.matcher(htmlText);
+			if (m.find()) {
+				String errMsg = m.group(1);
+				Log.e(TAG, "found error: " + errMsg);
+
+				if (errMsg.startsWith("recipient.")) {
+					throw new WebSMSException(context, R.string.error_recipient, errMsg);
+				}
+				if (errMsg.startsWith("message.")) {
+					throw new WebSMSException(context, R.string.error_text, errMsg);
+				}
+				if (errMsg.equals("duplicate_sms")) {
+					throw new WebSMSException(context, R.string.error_text, errMsg);
+				}
+
+				throw new WebSMSException(context, R.string.error_input, errMsg);
+			}
+		}
+		if (htmlText.contains("<okay")) {
+			// all good! there can be multiple okay for multiple recipients.
+			Log.d(TAG, "response message contains <okay>");
+		} else {
+			// no "okay" found, not good!
+			throw new WebSMSException(context, R.string.error_input, htmlText);
+
+		}
+
+	}
+
+	private static void readBalance(final ConnectorSpec cs, final String htmlText) {
+		// example: <smsResponse credit="20000">
+		Matcher m = PATTERN_CREDIT.matcher(htmlText);
+		if (m.find()) {
+
+			String credit = m.group(1);
+			Log.i(TAG, "found credit: " + credit);
+			cs.setBalance(credit);
 		}
 	}
 
@@ -125,62 +174,57 @@ public class ConnectorSmsBug extends Connector {
 		// do IO
 		try { // get Connection
 			final String text = command.getText();
-			final boolean checkOnly = (text == null || text.length() == 0);
-			final StringBuilder url = new StringBuilder();
 			final ConnectorSpec cs = this.getSpec(context);
-			final SharedPreferences p = PreferenceManager
-					.getDefaultSharedPreferences(context);
-			if (checkOnly) {
-				url.append(URL_BALANCE);
-			} else {
-				url.append(URL_SEND);
-			}
-			url.append("?Mobile_Number=");
-			url.append(Utils.getSender(context, command.getDefSender())
-					.replace("+", ""));
-			url.append("&Password=");
-			url.append(p.getString(Preferences.PREFS_PASSWORD, ""));
+			final SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(context);
 
-			if (!checkOnly) {
-				url.append("&Message=");
-				url.append(URLEncoder.encode(text));
-				url.append("&To_Numbers=");
-				url.append(Utils.joinRecipientsNumbers(command.getRecipients(),
-						",", true));
-			}
-			// send data
-			HttpResponse response = Utils.getHttpClient(url.toString(), null,
-					null, null, null, false);
+			// prepair form to send
+			HttpOptions options = new HttpOptions("UTF-8");
+			options.url = URL_SEND;
+			List<BasicNameValuePair> formData = new ArrayList<BasicNameValuePair>();
+			// allow messages longer than 160 chars.
+			formData.add(new BasicNameValuePair("multimessage", "true"));
+			formData.add(new BasicNameValuePair("message", text));
+			formData.add(new BasicNameValuePair("recipient", Utils.joinRecipientsNumbers(
+					command.getRecipients(), ",", true)));
+			options.addFormParameter(formData);
+
+			// setup user/password
+			options.addBasicAuthHeader(p.getString(Preferences.PREFS_USER, ""),
+					p.getString(Preferences.PREFS_PASSWORD, ""));
+			options.trustAll = true;
+
+			// send message
+			HttpResponse response = Utils.getHttpClient(options);
+
+			// evaluate response
 			int resp = response.getStatusLine().getStatusCode();
 			if (resp != HttpURLConnection.HTTP_OK) {
 				this.checkReturnCode(context, resp);
-				throw new WebSMSException(context, R.string.error_http, " "
-						+ resp);
+				throw new WebSMSException(context, R.string.error_http, " " + resp);
 			}
-			String htmlText = Utils.stream2str(
-					response.getEntity().getContent()).trim();
-			String[] lines = htmlText.split("\n");
+
+			/*
+			 * example response: <?xml version="1.0" encoding="ISO-8859-1"?>
+			 * <!DOCTYPE smsResponse PUBLIC "smsResponse"
+			 * "http://messenger.handybutler.ch/dtd/smsResponse.dtd">
+			 * <smsResponse credit="20000"> <okay id="53911:41796481111"
+			 * notiflink=
+			 * "https://www.sms- club.ch/websms/sclub/statusxml.do?msgid=53911&msisdn=+41796481111"
+			 * /> </smsResponse>
+			 */
+			String htmlText = Utils.stream2str(response.getEntity().getContent()).trim();
 			Log.d(TAG, "--HTTP RESPONSE--");
 			Log.d(TAG, htmlText);
 			Log.d(TAG, "--HTTP RESPONSE--");
+
+			readBalance(cs, htmlText);
+			checkForErrors(context, htmlText);
+
 			htmlText = null;
-			for (String s : lines) {
-				if (s.startsWith("Kontostand: ")) {
-					cs.setBalance(s.split(" ")[1].trim() + "\u20AC");
-				}
-			}
 		} catch (IOException e) {
 			Log.e(TAG, null, e);
 			throw new WebSMSException(e.getMessage());
 		}
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	protected final void doUpdate(final Context context, final Intent intent) {
-		this.sendData(context, new ConnectorCommand(intent));
 	}
 
 	/**
